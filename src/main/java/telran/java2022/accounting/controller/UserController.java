90cc8b5a00ad53f723cc10aca2335e7b25ecbd96
Anastasiya <anseair@gmail.com>
2023-05-28 23:48:18 +0300

    add cross origin

--- src/main/java/telran/java2022/accounting/controller/UserController.java
+++ src/main/java/telran/java2022/accounting/controller/UserController.java
@@ -2,6 +2,7 @@ package telran.java2022.accounting.controller;
 
 import java.security.Principal;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,36 +25,43 @@ import telran.java2022.accounting.service.UserService;
 public class UserController {
 	final UserService service;
 	
+	@CrossOrigin
 	@PostMapping("/register")
 	public UserDto register(@RequestBody UserRegisterDto userRegisterDto) {
 		return service.register(userRegisterDto);
 	}
 	
+	@CrossOrigin
 	@PostMapping("/login")
 	public UserDto login(Principal principal) {
 		return service.login(principal.getName());
 	}
 	
+	@CrossOrigin
 	@DeleteMapping("/user/{login}")
 	public UserDto deleteUser(@PathVariable String login) {
 		return service.deleteUser(login);
 	}
 	
+	@CrossOrigin
 	@PutMapping("/user/{login}")
 	public UserDto updateUser(@PathVariable String login, @RequestBody UserUpdateDto userUpdateDto) {
 		return service.updateUser(login, userUpdateDto);
 	}
 	
+	@CrossOrigin
 	@PutMapping("/user/{login}/role/{role}")
 	public RolesChangeDto addRole(@PathVariable String login, @PathVariable String role) {
 		return service.changeRoles(login, role, true);
 	}
 	
+	@CrossOrigin
 	@DeleteMapping("/user/{login}/role/{role}")
 	public RolesChangeDto deleteRole(@PathVariable String login, @PathVariable String role) {
 		return service.changeRoles(login, role, false);
 	}
 	
+	@CrossOrigin
 	@PutMapping("/changePassword/user/{login}")
 	public void changePassword(@PathVariable String login, @RequestHeader("X-Password") String newPassword) {
 		service.changePassword(login, newPassword);
